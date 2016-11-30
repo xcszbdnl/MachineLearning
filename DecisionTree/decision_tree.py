db765488bd38fc96045f108efacacac08ddfbe2c
xcszbdnl <xcsbdnl@gmail.com>
2016-11-30 23:34:23 +0800

    decision tree start

---
+++ DecisionTree/decision_tree.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+
+class DecsionNode:
+    pass
+
+def create_data():
+    train_data=[['slashdot', 'USA', 'yes', 18, 'None'],
+        ['google', 'France', 'yes', 23, 'Premium'],
+        ['digg', 'USA', 'yes', 24, 'Basic'],
+        ['kiwitobes', 'France', 'yes', 23, 'Basic'],
+        ['google', 'UK', 'no', 21, 'Premium'],
+        ['(direct)', 'New Zealand', 'no', 12, 'None'],
+        ['(direct)', 'UK', 'no', 21, 'Basic'],
+        ['google', 'USA', 'no', 24, 'Premium'],
+        ['slashdot', 'France', 'yes', 19, 'None'],
+        ['digg', 'USA', 'no', 18, 'None'],
+        ['google', 'UK', 'no', 18, 'None'],
+        ['kiwitobes', 'UK', 'no', 19, 'None'],
+        ['digg', 'New Zealand', 'yes', 12, 'Basic'],
+        ['slashdot', 'UK', 'no', 21, 'None'],
+        ['google', 'UK', 'yes', 18, 'Basic'],
+        ['kiwitobes', 'France', 'yes', 19, 'Basic']]
+    attribute_name = ['Referer', 'Country', 'Read FAQ', '# of webpages visited']
+    return train_data, attribute_name
+
+def build_tree():
+    pass
+
+
+def plot_tree():
+    pass
+
+def decision_tree():
+    train_data, attribute_name = create_data()
+    build_tree(train_data, attribute_name)
+    plot_tree()
+
+
+if __name__ == '__main__':
+    decision_tree()
