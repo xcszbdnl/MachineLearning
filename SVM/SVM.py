e84c65b1d46b7388ee571539009ac90b6a9deba1
xcszbdnl <xcsbdnl@gmail.com>
2017-03-07 00:08:45 +0800

    svm bug modified

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-SEPARABLE = False
+SEPARABLE = True
 GEN_W = 1
 GEN_B = 0
 LOW = -25
@@ -71,7 +71,7 @@ def select_j(index, e_i, param_alpha, b, x_train, y_train, e_cache):
     e_cache[index, :] = [1, e_i]
     valid_index = np.nonzero(e_cache[:, 0])[0]
     if len(valid_index) > 1:
-        for k in range(valid_index):
+        for k in valid_index:
             if k == index:
                 continue
             e_k = get_e(k, param_alpha, b, x_train, y_train)
@@ -119,9 +119,11 @@ def update_alpha(i, param_alpha, b, x_train, y_train, e_cache):
             L = max(0, alpha_j_old - alpha_i_old)
         if L == H:
             print 'L equals H'
+            return 0
         alpha_j_new = alpha_j_old + y_train[j] * (e_i - e_j) / eta
         alpha_j_new = clip_alpha(L, H, alpha_j_new)
         update_e(i, param_alpha, b, x_train, y_train, e_cache)
+        param_alpha[j] = alpha_j_new
         if abs(alpha_j_new - alpha_j_old) < TOLERANCE:
             print 'j not moving enough'
             return 0
@@ -136,6 +138,7 @@ def update_alpha(i, param_alpha, b, x_train, y_train, e_cache):
             b = b_2
         else:
             b = (b_1 + b_2) / 2
+        param_alpha[i] = alpha_i_new
         return 1
     return 0
 
@@ -145,8 +148,9 @@ def draw_line(param_alpha, b, x_train, y_train):
     for i in range(DATA_NUM):
         weight_w += param_alpha[i] * y_train[i] * x_train[i, :]
     k = -weight_w[0] / weight_w[1]
-    b /= weight_w[1]
-    r = np.sqrt(weight_w[0] ** 2 + weight_w[1] ** 2)
+    b /= -weight_w[1]
+    # r = np.sqrt(weight_w[0] ** 2 + weight_w[1] ** 2)
+    r = 1 / weight_w[1]
     point_x = [-20, 20]
     point_y = []
     point_y_up = []
@@ -160,9 +164,9 @@ def draw_line(param_alpha, b, x_train, y_train):
         point_y_down.append(y_2)
     cmap_bold = ListedColormap(['#FF0000', '#00FF00'])
     plt.scatter(x_train[:, 0], x_train[:, 1], c=y_train, cmap=cmap_bold)
-    plt.draw(point_x, point_y, 'b')
-    plt.draw(point_x, point_y_up, 'b--')
-    plt.draw(point_x, point_y_down, 'b--')
+    plt.plot(point_x, point_y, 'b')
+    plt.plot(point_x, point_y_up, 'b--')
+    plt.plot(point_x, point_y_down, 'b--')
     plt.show()
 
 
@@ -175,13 +179,14 @@ def svm():
     pair_changed = 0
     e_cache = np.zeros((DATA_NUM, 2))
     while iter_num < MAX_ITER and (entire_set is True or pair_changed > 0):
+        pair_changed = 0
         if entire_set is True:
             for i in range(DATA_NUM):
                 pair_changed += update_alpha(i, param_alpha, b, x_train, y_train, e_cache)
             print 'entire set, iter:%d, pair changed:%d' % (iter_num, pair_changed)
         else:
-            unbounded_index = np.nonzero(param_alpha > 0 * param_alpha < C)[0]
-            for i in range(unbounded_index):
+            unbounded_index = np.nonzero((param_alpha > 0) * (param_alpha < C))[0]
+            for i in unbounded_index:
                 pair_changed += update_alpha(i, param_alpha, b, x_train, y_train, e_cache)
                 print 'unbounded alpha, iter:%d, index:%d, pair changed:%d' % (iter_num, i, pair_changed)
         iter_num += 1
